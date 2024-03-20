1540dacbea0fdbc66e7eb88a442a76a4c8f6fa43
Min Iles <min.elliot48@gmail.com>
2024-03-20 20:43:42 +1300

    configured for deploy

--- server.js
+++ server.js
@@ -1,11 +1,1 @@
-const express = require('express')
-const app = express()
-const gameData = require('./routes/gameData')
-
-const port = process.env.PORT || '3001'
-
-app.use('/', gameData)
-
-app.listen(port, function () {
-  console.log(`Server running on ${port}`)
-})
+const gameData = require('./api/gameData')
